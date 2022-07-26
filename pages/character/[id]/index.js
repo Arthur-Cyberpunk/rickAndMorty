810186d988d564602544663aba6a8604f804ac58
Arthur Gabriel dos Santos <santos.santos86@hotmail.com>
2022-07-26 19:47:26 -0300

    removed styles

--- pages/character/[id]/index.js
+++ pages/character/[id]/index.js
@@ -81,39 +81,9 @@ export default function Character({ data }) {
           align-items: center;
         }
 
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
         .title a {
           color: #0070f3;
           text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
         }
 
         .title {
